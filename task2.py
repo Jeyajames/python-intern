6f34023a5f675ee0fb3d75ec892e884fe8a5a26d
Jeyajames <74947002+Jeyajames@users.noreply.github.com>
2020-11-24 05:22:33 -0800

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,17 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=b=c=5
+>>> a / 10
+0.5
+>>> b * 50
+250
+>>> c + 60
+65
+>>> 'james'.replace('m','g')
+'jages'
+>>> a,b=9,3.7
+>>> float(a)
+9.0
+>>> int(b)
+3
+>>> 
