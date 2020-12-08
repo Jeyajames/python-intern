58cf0249651e56f1623ee8caebb2fc4abeb43c69
Jeyajames <74947002+Jeyajames@users.noreply.github.com>
2020-12-08 08:51:31 -0800

    Add files via upload

---
+++ task 7.py
@@ -0,0 +1,20 @@
+#1.create a python module
+import mymodule
+mymodule.greeting("janu")
+
+#2.install pandas package
+#command-pip install pandas
+  
+#3.random number 1 to 100
+import random
+print("random number is",random.randint(1,100))
+
+#4.import system package and find the python path
+import os
+os.path
+
+#5.try to install a package and uninstall a package using pip
+
+#command-pip install camelcase
+#       -pip uninstall camelcase 
+
